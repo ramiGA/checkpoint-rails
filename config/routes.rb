ed48e1412c9f041df90509f2e632b1fef2f9ec6e
ramiGA <ramisn3@gmail.com>
2017-07-31 09:34:21 -0400

    check

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  get 'comment/index'
+
+  get 'comment/new'
+
+  get 'comment/show'
+
+  get 'comment/edit'
+
+  get 'comment/delete'
+
+  get 'post/index'
+
+  get 'post/new'
+
+  get 'post/show'
+
+  get 'post/edit'
+
+  get 'post/delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
